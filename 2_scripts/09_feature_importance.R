0563811d59dfdf590ef74b7e68166d00138d15de
chenyangsu <chen-yang.su@mail.mcgill.ca>
2023-06-12 16:29:09 -0400

    Update feature importance plots

--- 2_scripts/09_feature_importance.R
+++ 2_scripts/09_feature_importance.R
@@ -4,6 +4,7 @@ library(ggplot2)
 library(scales)
 library(dplyr)
 source("2_scripts/utility_functions.R")
+library(gridExtra)
 theme_set(theme_bw())
 
 intermediate_directory <- './3_intermediate/feature_importance/main_model/'
@@ -96,6 +97,10 @@ featimp_h_f <- fread("./3_intermediate/feature_importance/main_model/1RF_hgb_onl
 
 # featname_lookup <- fread("./1_data/feature_name_lookup.csv")
 
+
+
+
+
 ## featimp_hf_h ----
 
 plot_feature_importance <- function(df, path_to_save) {
@@ -109,51 +114,84 @@ plot_feature_importance <- function(df, path_to_save) {
             RMSPE_percentage_change := (res - bl_res),
             on=.(rpt, fold)]
   
+  df <- df %>%
+    mutate(
+      feature = case_when(
+        feature == "rbc_loss_last_12_months" ~ "RBC loss last 12 months",
+        feature == "rbc_loss_last_24_months" ~ "RBC loss last 24 months",
+        feature == "days_since_last_rbc_loss" ~ "Days since last RBC loss",
+        feature == "days_since_last_drbc_loss" ~ "Days since last DRBC loss",
+        feature == "cum_lifetime_donations" ~ "Cumulative lifetime donations",
+        feature == "index_hgb" ~ "Index HGB",
+        feature == "blood_type" ~ "Blood type",
+        feature == "index_ferritin" ~ "Index ferritin",
+        feature == "index_log_ferritin" ~ "Index log ferritin",
+        feature == "time_to_fu" ~ "Time to follow up",
+        feature == "sex" ~ "Sex",
+        feature == "age" ~ "Age",
+        TRUE ~ feature  # keep other elements unchanged
+      )
+    )
+  
+  
+  
   ggplot(df)+
     geom_boxplot(aes(x=reorder(feature, RMSPE_percentage_change, FUN = median), y=RMSPE_percentage_change))+
     coord_flip()+geom_hline(yintercept=0, color="red")+
     scale_y_continuous(name = "Change in RMSPE")+  #  (RMSPE - baseline RMPSE)
     xlab("")
   
-  ggsave(path_to_save,
-         width = 5, height = 6, units = "in")
+  # ggsave(path_to_save,
+  #        width = 5, height = 6, units = "in")
+  
+  fig <- ggplot(df)+
+    geom_boxplot(aes(x=reorder(feature, RMSPE_percentage_change, FUN = median), y=RMSPE_percentage_change))+
+    coord_flip()+geom_hline(yintercept=0, color="red")+
+    scale_y_continuous(name = "Change in RMSPE")+  #  (RMSPE - baseline RMPSE)
+    xlab("")
+  
+  return(fig)
 }
 
-plot_feature_importance(df=featimp_hf_h, path_to_save="./4_output/figs/feature_importance_hgb_ferr_pred_hgb.svg")
-plot_feature_importance(df=featimp_hf_f, path_to_save="./4_output/figs/feature_importance_hgb_ferr_pred_ferr.svg")
-plot_feature_importance(df=featimp_h_h, path_to_save="./4_output/figs/feature_importance_hgb_only_pred_hgb.svg")
-plot_feature_importance(df=featimp_h_f, path_to_save="./4_output/figs/feature_importance_hgb_only_pred_ferr.svg")
+fig_a <- plot_feature_importance(df=featimp_hf_h, path_to_save="./4_output/figs/fig2a_feature_importance_hgb_ferr_pred_hgb.svg")
+fig_b <- plot_feature_importance(df=featimp_h_h, path_to_save="./4_output/figs/fig2b_feature_importance_hgb_only_pred_hgb.svg")
+fig_c <- plot_feature_importance(df=featimp_hf_f, path_to_save="./4_output/figs/fig2c_feature_importance_hgb_ferr_pred_ferr.svg")
+fig_d <- plot_feature_importance(df=featimp_h_f, path_to_save="./4_output/figs/fig2d_feature_importance_hgb_only_pred_ferr.svg")
 
 
+combined_plot <- grid.arrange(fig_a, fig_b, fig_c, fig_d,
+                              ncol = 2, nrow = 2)
+
+ggsave(plot = combined_plot, "./4_output/figs/fig2_all.svg", width = 8, height = 6, units = "in")
 
 
 
 # ## Combined fig ----
-# featimp_all <- rbind(
-#   cbind(mod = "Hemoglobin and Ferritin predict hemoglobin", featimp_hf_h),
-#   cbind(mod = "Hemoglobin and Ferritin predict ferritin", featimp_hf_f),
-#   cbind(mod = "Hemoglobin only predict hemoglobin", featimp_h_h),
-#   cbind(mod = "Hemoglobin only predict ferritin", featimp_h_f)
-# )
-# 
-# baselines <- featimp_all[feature == "baseline"]
-# 
-# colnames(baselines)[5] <- paste0("bl_",colnames(baselines)[5])
-# featimp_all <-featimp_all[feature != "baseline"]
-# 
-# setDT(featimp_all)[baselines,
-#                    RMSPE_percentage_change := (res - bl_res),
-#                    on=.(mod, rpt, fold)]
-# 
-# 
-# 
-# ggplot(featimp_all)+
-#   geom_boxplot(aes(x=reorder(feature, RMSPE_percentage_change, FUN = median), y=RMSPE_percentage_change))+
-#   facet_grid(cols = vars(mod))+
-#   coord_flip()+geom_hline(yintercept=0, color="red")+
-#   scale_y_continuous(name = "Change in RMSPE")+
-#   xlab("")
-# 
-# ggsave("./4_output/figs/feat_imp_all.svg",
-#        width = 5, height = 4, units = "in")
+featimp_all <- rbind(
+  cbind(mod = "Hemoglobin and Ferritin predict hemoglobin", featimp_hf_h),
+  cbind(mod = "Hemoglobin and Ferritin predict ferritin", featimp_hf_f),
+  cbind(mod = "Hemoglobin only predict hemoglobin", featimp_h_h),
+  cbind(mod = "Hemoglobin only predict ferritin", featimp_h_f)
+)
+
+baselines <- featimp_all[feature == "baseline"]
+
+colnames(baselines)[5] <- paste0("bl_",colnames(baselines)[5])
+featimp_all <-featimp_all[feature != "baseline"]
+
+setDT(featimp_all)[baselines,
+                   RMSPE_percentage_change := (res - bl_res),
+                   on=.(mod, rpt, fold)]
+
+
+
+ggplot(featimp_all)+
+  geom_boxplot(aes(x=reorder(feature, RMSPE_percentage_change, FUN = median), y=RMSPE_percentage_change))+
+  facet_grid(cols = vars(mod))+
+  coord_flip()+geom_hline(yintercept=0, color="red")+
+  scale_y_continuous(name = "Change in RMSPE")+
+  xlab("")
+
+ggsave("./4_output/figs/feat_imp_all.svg",
+       width = 5, height = 4, units = "in")
 
